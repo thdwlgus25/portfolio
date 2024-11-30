1ac0d804aa5eba7ffe4999247923a0936a4a6ffe
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-12-01 01:14:50 +0900

    Feat: header 아이콘 사용자 링크 추가

--- src/main/java/com/jihyun/portfolio/member/controller/HeaderController.java
+++ src/main/java/com/jihyun/portfolio/member/controller/HeaderController.java
@@ -2,6 +2,8 @@ package com.jihyun.portfolio.member.controller;
 
 import com.jihyun.portfolio.member.service.MemberAddService;
 import com.jihyun.portfolio.profile.dto.MyProfileDto;
+import com.jihyun.portfolio.profile.dto.MySocialDto;
+import com.jihyun.portfolio.social.service.SocialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class HeaderController {
 
     private final MemberAddService memberAddService;
+    private final SocialService socialService;
 
     @ModelAttribute("profile")
     public MyProfileDto addMemberNameToHeader() {
@@ -27,6 +30,18 @@ public class HeaderController {
         // 로그인된 사용자의 이메일로 사용자 정보 가져오기
         String memberEmail = authentication.getName();
         return memberAddService.getMyProfile(memberEmail);
+    }
+
+    @ModelAttribute("socialData")
+    public MySocialDto addSocialLinksToHeader() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
+            return null; // 로그아웃 상태
+        }
+
+        String memberEmail = authentication.getName();
+        return socialService.getSocialData(memberEmail);
     }
 
 }
