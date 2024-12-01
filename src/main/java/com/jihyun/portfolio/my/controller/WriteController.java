6445ff5ba761949fe890194c6ef4f824b262ad21
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-12-02 01:31:44 +0900

    Feat: 작성자 이름 가져오기

--- src/main/java/com/jihyun/portfolio/my/controller/WriteController.java
+++ src/main/java/com/jihyun/portfolio/my/controller/WriteController.java
@@ -1,7 +1,11 @@
 package com.jihyun.portfolio.my.controller;
 
 import com.jihyun.portfolio.category.service.CategoryService;
+import com.jihyun.portfolio.member.service.MemberAddService;
+import com.jihyun.portfolio.profile.dto.MyProfileDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +15,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WriteController {
 
     private final CategoryService categoryService;
+    private final MemberAddService memberAddService;
 
     @GetMapping("/write")
     public String write(Model model) {
 
         // 카테고리
         model.addAttribute("categories", categoryService.getAllCategory());
+
+        // 글쓴이 이름 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String memberEmail = authentication.getName();
+        MyProfileDto profile = memberAddService.getMyProfile(memberEmail);
+        model.addAttribute("profile", profile);
+
         return "page/my/Write";
     }
 
